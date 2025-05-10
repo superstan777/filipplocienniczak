81eef0a1cd688eaca7d20c553c25e50f2263c210
superstan777 <viir4d@gmail.com>
2025-05-10 17:29:29 +0200

    Copy update

--- src/app/sections/Reviews.tsx
+++ src/app/sections/Reviews.tsx
@@ -9,69 +9,9 @@ import {
   CarouselItem,
   CarouselApi,
 } from "../components/ui/carousel";
+import { reviews } from "../utility/copy";
 
 export const Reviews = () => {
-  const reviewsData = [
-    {
-      name: "Mateusz Żyła",
-      avatar: "",
-      ago: "3 tygodnie temu",
-      review:
-        "Z Panem Filipem uzyskaliśmy kredyt 2% po długim i zaciętym boju z bankiem pekao s.a. Na początku września 2023 złożyliśmy za pośrednictwem Pana Filipa wniosek o kredyt 2% do banku Pekao S.A. oraz PKO BP. Przez parę miesięcy nic się w temacie nie działo, uzyskaliśmy pozytywna decyzję finansową o czym zostaliśmy przez Filipa poinformowani. Że względu na to że nieruchomość została oddana do użytku dopiero na koniec grudnia (oraz przez naciski banków by procesować wniosek tylko w jednym banku) musieliśmy zrezygnować z procesowania wniosku w PKO BP. I tutaj zaczęło się piekło - pracownik banku Pekao źle wprowadził nasz wniosek do systemu (na szczęście Pan Filip miał kopie tego co zostało złożone). Bank przez cały styczeń i luty mydlił oczy że będą w stanie to naprawić. W międzyczasie wygasła nam decyzja finansową. Filip pomógł nam napisać reklamację oraz doradził by udać się do prawnika (którego zorganizował). Ostatecznie we własnym zakresie znaleźliśmy innego prawnika oraz wciągnęliśmy do gry rzecznika  finansowego. Ostatecznie  po wielu miesiącach walki przy wsparciu Filipa, prawnika oraz rzecznika finansowego otrzymalismy kredyt 2%.",
-    },
-    {
-      name: "Klaudia Małaczyńska",
-      avatar: "",
-      ago: "miesiąc temu",
-      review: `Trafiliśmy do Filipa z polecenia i jeśli zajdzie taka potrzeba, na pewno wrócimy.
-      Filip przeprowadził nas przez cały proces kredytu i pomagał na każdej możliwej płaszczyźnie. Jasno, szybko (na tyle ile banki pozwalają :) ) i bardzo klarownie.
-      Serdecznie polecamy !`,
-    },
-    {
-      name: "Patryk Śródecki",
-      avatar: "",
-      ago: "miesiąc temu",
-      review:
-        "Pełen profesjonalizm i stu procentowe zaangażowanie Pana Filipa - jest to gwarancja zadowolenia. Pan Filip dał się poznać jako osoba o wysokiej kulturze osobistej i dużej wiedzy. Gorąco polecam !",
-    },
-    {
-      name: "Daria Weryńska",
-      avatar: "",
-      ago: "2 miesiące temu",
-      review:
-        "Szczerze polecam współpracę! Przez cały proces uzyskiwania kredytu hipotecznego czułam się prowadzona rzetelnie i kompleksowo, a jednak z uśmiechem :) Filip służył wsparciem, poradą i odpowiedziami nawet o dziwnych porach. Ma duże doświadczenie i poczucie humoru 🙂…",
-    },
-    {
-      name: "Da wid",
-      avatar: "",
-      ago: "4 miesiące temu",
-      review:
-        "Ekspresowo pomógł nam wybrać najlepszą ofertę kredytową i uzyskać finansowanie. Pełen profesjonalizm i terminowość, wszystkie wnioski obsłużone bardzo szybko. Problemy po stronie banku itp praktycznie Ciebie nie obchodzą, Filip czuwa nad wszystkim.",
-    },
-    {
-      name: "Magda S",
-      avatar: "",
-      ago: "4 miesiące temu",
-      review:
-        "Do Pana Filipa trafiliśmy z polecenia i był to strzał w 10-tkę. Pomaga i wspiera w każdym etapie procesu kredytowego. Odpowiada zawsze na każde pytanie. Cechuje się ogromnym profesjonalizmem, przy tym jest też bardzo sympatycznym człowiekiem. Jeśli ktoś - tak jak ja z moim mężem - w temacie kredytów jest totalnym laikiem, pan Filip zaopiekuje się nim w 100%. Serdecznie polecam :)",
-    },
-    {
-      name: "Marcin Arndt",
-      avatar: "",
-      ago: "5 miesięcy temu",
-      review:
-        "Pan Filip to w pełni profesjonalny oraz posiadający ogromną wiedzę na temat kredytów doradca. Dużym plusem naszej współpracy było to, że Pan Filip zajął się praktycznie wszystkim, bez mojego zbytniego zaangażowania. Polecam i dziękuję!",
-    },
-
-    {
-      name: "Patrycja Latecka",
-      avatar: "",
-      ago: "6 miesięcy temu",
-      review:
-        "Polecamy z mężem Pana Filipa z całego serca! Świetny doradca kredytowy, pomaga i wspiera na każdym kroku. Jest zawsze przygotowany, konkretny i zaangażowany. Same plusy! Dziękujemy raz jeszcze za przejście z nami przez cały proces, nie moglibyśmy sobie wyobrazić lepszego eksperta ☺️ będziemy polecali każdemu!",
-    },
-  ];
-
   const reviewsRef = useRef(null!);
   const [api, setApi] = useState<CarouselApi>();
   const [wasScrolled, setWasScrolled] = useState(false);
@@ -89,7 +29,7 @@ export const Reviews = () => {
   //
 
   const renderReviews = () => {
-    return reviewsData.map((item, index) => {
+    return reviews.map((item, index) => {
       return (
         <CarouselItem
           className="basis-1/1 lg:basis-1/2 xl:basis-1/3  lg:pl-[1.25em] "
---
+++ src/app/utility/copy.ts
@@ -0,0 +1,124 @@
+// export const reviews = [
+//   {
+//     name: "Mateusz Żyła",
+//     avatar: "",
+//     ago: "3 tygodnie temu",
+//     review:
+//       "Z Panem Filipem uzyskaliśmy kredyt 2% po długim i zaciętym boju z bankiem pekao s.a. Na początku września 2023 złożyliśmy za pośrednictwem Pana Filipa wniosek o kredyt 2% do banku Pekao S.A. oraz PKO BP. Przez parę miesięcy nic się w temacie nie działo, uzyskaliśmy pozytywna decyzję finansową o czym zostaliśmy przez Filipa poinformowani. Że względu na to że nieruchomość została oddana do użytku dopiero na koniec grudnia (oraz przez naciski banków by procesować wniosek tylko w jednym banku) musieliśmy zrezygnować z procesowania wniosku w PKO BP. I tutaj zaczęło się piekło - pracownik banku Pekao źle wprowadził nasz wniosek do systemu (na szczęście Pan Filip miał kopie tego co zostało złożone). Bank przez cały styczeń i luty mydlił oczy że będą w stanie to naprawić. W międzyczasie wygasła nam decyzja finansową. Filip pomógł nam napisać reklamację oraz doradził by udać się do prawnika (którego zorganizował). Ostatecznie we własnym zakresie znaleźliśmy innego prawnika oraz wciągnęliśmy do gry rzecznika  finansowego. Ostatecznie  po wielu miesiącach walki przy wsparciu Filipa, prawnika oraz rzecznika finansowego otrzymalismy kredyt 2%.",
+//   },
+//   {
+//     name: "Klaudia Małaczyńska",
+//     avatar: "",
+//     ago: "miesiąc temu",
+//     review: `Trafiliśmy do Filipa z polecenia i jeśli zajdzie taka potrzeba, na pewno wrócimy.
+//       Filip przeprowadził nas przez cały proces kredytu i pomagał na każdej możliwej płaszczyźnie. Jasno, szybko (na tyle ile banki pozwalają :) ) i bardzo klarownie.
+//       Serdecznie polecamy !`,
+//   },
+//   {
+//     name: "Patryk Śródecki",
+//     avatar: "",
+//     ago: "miesiąc temu",
+//     review:
+//       "Pełen profesjonalizm i stu procentowe zaangażowanie Pana Filipa - jest to gwarancja zadowolenia. Pan Filip dał się poznać jako osoba o wysokiej kulturze osobistej i dużej wiedzy. Gorąco polecam !",
+//   },
+//   {
+//     name: "Daria Weryńska",
+//     avatar: "",
+//     ago: "2 miesiące temu",
+//     review:
+//       "Szczerze polecam współpracę! Przez cały proces uzyskiwania kredytu hipotecznego czułam się prowadzona rzetelnie i kompleksowo, a jednak z uśmiechem :) Filip służył wsparciem, poradą i odpowiedziami nawet o dziwnych porach. Ma duże doświadczenie i poczucie humoru 🙂…",
+//   },
+//   {
+//     name: "Da wid",
+//     avatar: "",
+//     ago: "4 miesiące temu",
+//     review:
+//       "Ekspresowo pomógł nam wybrać najlepszą ofertę kredytową i uzyskać finansowanie. Pełen profesjonalizm i terminowość, wszystkie wnioski obsłużone bardzo szybko. Problemy po stronie banku itp praktycznie Ciebie nie obchodzą, Filip czuwa nad wszystkim.",
+//   },
+//   {
+//     name: "Magda S",
+//     avatar: "",
+//     ago: "4 miesiące temu",
+//     review:
+//       "Do Pana Filipa trafiliśmy z polecenia i był to strzał w 10-tkę. Pomaga i wspiera w każdym etapie procesu kredytowego. Odpowiada zawsze na każde pytanie. Cechuje się ogromnym profesjonalizmem, przy tym jest też bardzo sympatycznym człowiekiem. Jeśli ktoś - tak jak ja z moim mężem - w temacie kredytów jest totalnym laikiem, pan Filip zaopiekuje się nim w 100%. Serdecznie polecam :)",
+//   },
+//   {
+//     name: "Marcin Arndt",
+//     avatar: "",
+//     ago: "5 miesięcy temu",
+//     review:
+//       "Pan Filip to w pełni profesjonalny oraz posiadający ogromną wiedzę na temat kredytów doradca. Dużym plusem naszej współpracy było to, że Pan Filip zajął się praktycznie wszystkim, bez mojego zbytniego zaangażowania. Polecam i dziękuję!",
+//   },
+
+//   {
+//     name: "Patrycja Latecka",
+//     avatar: "",
+//     ago: "6 miesięcy temu",
+//     review:
+//       "Polecamy z mężem Pana Filipa z całego serca! Świetny doradca kredytowy, pomaga i wspiera na każdym kroku. Jest zawsze przygotowany, konkretny i zaangażowany. Same plusy! Dziękujemy raz jeszcze za przejście z nami przez cały proces, nie moglibyśmy sobie wyobrazić lepszego eksperta ☺️ będziemy polecali każdemu!",
+//   },
+// ];
+
+export const reviews = [
+  {
+    name: "travelman",
+    avatar: "",
+    ago: "3 tygodnie temu",
+    review:
+      "Polecam Pana Filipa, cały proces kredytowy przechodzi się łatwo i dzięki niemu nie trzeba osobiście kontaktować się ze wszystkimi bankami w celu analizy warunków. Profesjonalna pomoc w bardzo dobrej atmosferze.",
+  },
+  {
+    name: "Dawid Pozorski",
+    avatar: "",
+    ago: "miesiąc temu",
+    review:
+      "Z całego serca polecam! Filip to prawdziwy fachowiec w swojej dziedzinie! Pomógł nam w uzyskaniu kredytu na dom na bardzo korzystnych warunkach. Cały proces przebiegał szybko i sprawnie, wszelkie niejasności były rozwiązywane w trybie natychmiastowym. Ponadto Filip to bardzo sympatyczny gość z którym współpraca to czysta przyjemność, gorąco polecam! Gwarancja zadowolenia!",
+  },
+  {
+    name: "Marta Lech-Pozorska",
+    avatar: "",
+    ago: "miesiąc temu",
+    review:
+      "Bardzo, bardzo polecam! Przez cały proces kredytowania czuliśmy się „zaopiekowani”. Byliśmy w stałym kontakcie, na każdym etapie mogliśmy liczyć na pomoc oraz ogrom wiedzy. Ciesze się, że trafiliśmy w ręce Filipa",
+  },
+  {
+    name: "Joanna Wachowiak",
+    avatar: "",
+    ago: "3 miesiące temu",
+    review:
+      "Polecam Pana Filipa! Pełny profesjonalizm, rzetelna pomoc, komunikatywność i... uzyskany kredyt. Czy można chcieć więcej? Polecam!",
+  },
+  {
+    name: "Jakub Czechowicz",
+    avatar: "",
+    ago: "3 miesiące temu",
+    review:
+      "Filip Płócienniczak to wyjątkowy ekspert kredytowy, który z profesjonalizmem i zaangażowaniem pomógł mi uzyskać korzystny kredyt, dostosowany do moich potrzeb. Dzięki jego wsparciu i klarownemu przeprowadzeniu przez cały proces kredytowy, mogłem czuć się pewnie na każdym etapie.",
+  },
+  {
+    name: "Maciej Szymański",
+    avatar: "",
+    ago: "5 miesięcy temu",
+    review:
+      "Gorąco polecam. Filip pomagał nam w uzyskaniu kredytu na zakup domu, dzięki jego radom cały temat poszedł bardzo sprawnie i szybko. Jeśli ktoś szuka doradcy kredytowego w Poznaniu to zachęcam do kontaktu z Filipem.",
+  },
+  {
+    name: "Joanna Banasik",
+    avatar: "",
+    ago: "6 miesięcy temu",
+    review: `Polecam! Szybko i sprawnie! :) Pomino pewnych trudności ze strony banku mogłam liczyć na pełne wsparcie oraz szybkie znalezienie rozwiązań alternatywnych, co pozwoliło mi ostatecznie sfinalizować zakup w wymaganym terminie. Sprawna komunikacja mailowa/SMS/telefoniczna. Dodatkowo znalezienie terminu w banku celem podpisania umowy w ciągu jednego dnia, przed Wszystkimi Świętymi - szacunek! :) Pełen profesjonalizm! Jeśli w przyszłości będę rozważać zakup nieruchomości, to tylko z Panem Filipem.
+Szczerze POLECAM! :)`,
+  },
+  {
+    name: "Kacper Maksymilian Brembor",
+    avatar: "",
+    ago: "8 miesięcy temu",
+    review: `Z całego serca polecam Pana Filipa jako doradcę kredytowego. Współpracowaliśmy od kwietnia 2024 roku do końca sierpnia 2024 roku i mogę śmiało powiedzieć, że Pan Filip wykazał się niesamowitym profesjonalizmem, wiedzą oraz zaangażowaniem na każdym etapie naszej współpracy.
+
+Nasza sytuacja była szczególnie trudna, ponieważ po pierwszej odmowie z jednego z banków, jak i drugiego banku ścięcie kwoty kredytu, stwierdziliśmy że zmienimy nieruchomość z nieruchomości w stanie deweloperskiej na dom w stanie budowy, co wiązało się z dodatkowymi wyzwaniami. Ostatni wniosek kredytowy, złożony na początku lipca, dotyczył domu w trakcie budowy, który miał być wykończony "pod klucz". Komplikacji dodawał fakt, że właściciele nieruchomości przebywali za granicą, co wymagało skoordynowania wielu formalności przy mojej pomocy oraz pełnomocnika właścicieli.
+
+Pan Filip poradził sobie z tymi wyzwaniami znakomicie. Dzięki jego wsparciu i wiedzy udało się sfinalizować cały proces, mimo skomplikowanej sytuacji. Był zawsze dostępny, cierpliwie odpowiadał na wszystkie pytania, a było ich sporo szczególnie na końcu procesu który się przeciągał prawie 40 dni roboczych, a jego podejście sprawiło, że czuliśmy się pewnie i spokojnie, wiedząc, że jesteśmy w dobrych rękach.
+
+Jeśli szukasz profesjonalnego, rzetelnego i zaangażowanego doradcy kredytowego, Pan Filip to osoba, której możesz zaufać. Gorąco polecam!`,
+  },
+];
